9eec60bfe403d3cb9faff8502452c41d78ffb43d
Jihoon Lee <jihoonlee.in@gmail.com>
2013-04-20 09:47:22 +0900

    updates

--- www/js/annotator/circle.js
+++ www/js/annotator/circle.js
@@ -33,15 +33,39 @@ ANNOTATOR.Circle= function(options) {
     return pose;
   }
 
+  var createCircle = function(x,y,radius) {
+    var circle = new createjs.Shape();
+    circle.graphics.beginFill("red").drawCircle(0,0,radius);
+    circle.x = x;
+    circle.y = y;
+
+    return circle;
+  }
+
   this.rootObject.addEventListener('mousedown',function(event) {
     // convert to ROS coordinates
     var pose = getPose(event);
     var mouse_ondrag = false;
+    var circle;
 
     event.onMouseMove = function(move_event) {
-      var move_pose = getPose(event);
+      var move_pose = getPose(move_event);
 
-      mouse_ondrag = true;
+      move_pose.position.subtract(pose.position);
+      var mag = Math.sqrt(move_pose.position.x * move_pose.position.x  + move_pose.position.y * move_pose.position.y);
+
+      if(mouse_ondrag) {
+        circle.graphics.beginFill("red").drawCircle(0,0,mag);
+        circle.x = pose.position.x - map_origin.position.x;
+        circle.y = -(pose.position.y - map_origin.position.y);
+      }
+      else {
+        var x = pose.position.x - map_origin.position.x;
+        var y = -(pose.position.y - map_origin.position.y);
+        circle = createCircle(x,y,mag);
+        mouse_ondrag = true;
+        that.rootObject.addChild(circle);
+      }
       console.log('move');
 
     }
@@ -52,10 +76,8 @@ ANNOTATOR.Circle= function(options) {
       }
     }
 
+
   });
-
-
-
 
 
   this.rootObject.addEventListener('mouseup',function(event) {
